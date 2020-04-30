e03a585af2ec1c52eac51f4fdd70c1701e9fbba7
vagrant <vagrant@vagrant.vm>
2020-04-30 05:26:29 +0000

    initiating calculator

--- calculator.py
+++ calculator.py
@@ -2,6 +2,49 @@
 
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
+while True:
+    user_input = input('Enter your equation > ')
+    tokens = user_input.split()
 
+    if 'q' in tokens:
+        print('You will exit.')
+        break
 
-print("1+2")
+    operator = tokens[0]
+    num1 = tokens[1]
+
+    if len(tokens) == 3:
+        num2 = tokens[2]
+
+    if len(tokens) > 3:
+        num3 = tokens[3]
+
+    result = None
+
+    
+    if operator == '+':
+        result = add(float(num1), float(num2))
+
+    elif operator == '-':
+        result = subtract(float(num1), float(num2))
+
+    elif operator == '*':
+        result = multiply(float(num1), float(num2))
+
+    elif operator == '/':
+        result = divide(float(num1), float(num2))
+
+    elif operator == 'square':
+        result = square(float(num1))
+
+    elif operator == 'cube':
+        result = cube(float(num1))
+
+    elif operator == 'pow':
+        result = power(float(num1), float(num2))
+
+    elif operator == 'mod':
+        result = mod(float(num1), float(num2))
+
+    print(result)
+
